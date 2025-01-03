bb01b24f0564ab8841911ff0cbdf42a195cb65e1
Vinicius de Alencar Freire <alencarfreire@gmail.com>
2025-01-03 17:42:30 -0300

    test(input): added more two tests in component

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -1,8 +1,29 @@
-import { render } from "@testing-library/react-native";
+import { fireEvent, render } from "@testing-library/react-native";
 import Input from "./index";
 
 describe("Component: Input", () => {
   it("should be render component", () => {
     render(<Input />);
+  });
+  it("should be render component with placeholder prop", () => {
+    const placeholder = "Clique aqui";
+    const { getByPlaceholderText } = render(
+      <Input placeholder={placeholder} />
+    );
+    expect(getByPlaceholderText(placeholder)).toBeTruthy();
+  });
+
+  it("should update the value when typing", () => {
+    const onChangeText = jest.fn();
+    const initialValue = "Inicial";
+    const newValue = "Novo valor";
+
+    const { getByDisplayValue } = render(
+      <Input value={initialValue} onChangeText={onChangeText} />
+    );
+
+    const input = getByDisplayValue(initialValue);
+    fireEvent.changeText(input, newValue);
+    expect(onChangeText).toHaveBeenCalledWith(newValue);
   });
 });
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,12 +4,14 @@ import * as S from "./styles";
 type Props = {
   placeholder?: string;
   value?: string;
+  onChangeText?: () => void;
 };
 
-export default function Input({ placeholder, value }: Props) {
+export default function Input({ placeholder, value, onChangeText }: Props) {
   const [isFocused, setIsFocused] = useState(false);
   return (
     <S.TextInput
+      onChangeText={onChangeText}
       isFocused={isFocused}
       onFocus={() => setIsFocused(true)}
       onBlur={() => setIsFocused(false)}
