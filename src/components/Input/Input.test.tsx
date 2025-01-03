58038332a68abd8dc2d970e3c7cd192140cf27ff
Vinicius de Alencar Freire <alencarfreire@gmail.com>
2025-01-03 15:41:05 -0300

    test(input): added test render input

---
+++ src/components/Input/Input.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react-native";
+import Input from "./index";
+
+describe("Component: Input", () => {
+  it("should be render component", () => {
+    render(<Input />);
+  });
+});
